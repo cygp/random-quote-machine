e9ee30ef94c278272aa113c6fd2b2e4eb67c217b
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-09-15 16:06:57 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 (function() {
 	var tweetLink = "https://twitter.com/intent/tweet?text=";
 	var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
+	//get quoute
 	function getQuote() {
 	    fetch(quoteUrl, { cache: "no-store" })
 	        .then(function(resp) {
@@ -9,6 +9,7 @@
 	        })
 	        .then(createTweet);
 	}
+	// creating tweet
 	function createTweet(input) {
 	    var data = input[0];
 	    var dataElement = document.createElement('div');
@@ -19,6 +20,22 @@
 	    if (!quoteAuthor.length) {
 	        quoteAuthor = "Unknown author";
 	    }
+		var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+		//checking quote length
+		if (tweetText.length > 140) {
+		    getQuote();
+		} else {
+		    var tweet = tweetLink + encodeURIComponent(tweetText);
+		    document.querySelector('.quote').innerText = quoteText;
+		    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+		    document.querySelector('.tweet').setAttribute('href', tweet);
+		}
 	}
+	document.addEventListener('DOMContentLoaded', function() {
+	    getQuote();
+	    document.querySelector('.trigger').addEventListener('click', function() {
+	        getQuote();
+	    });
+	});
 
 })();
